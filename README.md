2336104c68aeb1ce8d38762bc87b45d654353570
Tadam1999 <toth99adam@gmail.com>
2021-07-27 18:39:25 +0200

    A Mozizós feature első commit-ja. Egyelőre a lejátszást elindító üzenetet, valamint annak validálását tartalmazza. Tesztelve még nem volt, a héten erre is sor kerül.
    Bővítettem a README.md-t, az ehhez szükséges "dokumentációval".

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # TothLaci
-A HaT Discord Botja
+# A HaT Discord Botja
+
+# Mozizós feature
+
+### A végső cél, hogy egy, vagy több csatornán lehessen bizonyos formátumú videókat lejátszani. Ezen videók lejátszását, majd felhasználók egy csoportja vezérelheti egy erre külön létrehozott csatornáról.
+
+Jelen állás szerint szükség van egy "Mozi_gephaz" csatornára, ahol "Vetito" ranggal rendelkező felhasználók indíthatják
+majd el egy-egy videó lejátszását. Egyenlőre csak egy csatornán, persze később párhuzamosan több csatornával is kéne működnie.
+
+Az indító üzenetnek a következő formátumúnak kell lennie:
+ - Két részből álljon, középen pontosan egy szóközzel elválasztva.
+ - Az üzenet első fele pontosan "!play" kell, hogy legyen.
+ - Az üzenet második fele a lejátszani kívánt file elérési útvonalát tartalmazza.
--- TothLaci.py
+++ TothLaci.py
@@ -1,45 +1,33 @@
-# import os
-
-# import discord
-
-# TOKEN = os.getenv('DISCORD_TOKEN')
-
-# client = discord.Client()
-
-# @client.event
-# async def on_member_join():
-#     print(f'{client.user} has connected to Discord!')
-
-# client.run(TOKEN)
-import discord
-import time
 import os
-from datetime import datetime
+import pathlib
+import discord
 
-TOKEN= os.getenv("DISCORD_TOKEN")
-
+TOKEN = os.getenv("DISCORD_TOKEN")
 client = discord.Client()
 
-@client.event
-async def on_ready():
-    print(client.guilds)
-    text_channels = client.guilds[0].text_channels
-    channel = text_channels[0]
-    # await channel.send("Szasztok skaccok")
 
 @client.event
 async def on_message(message):
     content = message.content
     channel = message.channel
-    print(channel.type.value)
-    if channel.type.value == 1:
-        if content.split(' ')[0].isnumeric():
-            time.sleep(int(content.split(' ')[0]))
-            await message.reply(" ".join(content.split(' ')[1:]))
-            
-@client.event
-async def on_member_update(old, new):
-    pass
+    sender = message.author
+    # Eloszor azt nezzuk meg, hogy a megfelelo joggal rendelkezo felhasznalo kuldte-e az uzenetet.
+    # Es hogy a megfelelo csatornara.
+    if sender.roles.contains("Vetito") and channel == "Mozi_gephaz":
+        # Most nezzuk meg, hogy az uzenet megfelelo formatumu-e.
+        # kezdodjon '!play' legyen kozepen egy space, majd a file-hoz vezeto eleresi utvonal.
+        if " " in content and content.split(' ')[0] == "!play" and pathlib.is_file(content.split(' ')[1]):
+            # Itt meg lehet a file formatumat valahogy validalni kene, eddig csak a letezest neztem meg ezzel a feltetellel.
+            # A lejatszas elott megkene nezni, hogy van-e folyamatban eppen masik vetites. (Ez majd kesobb kene, de problemat jelenthet majd.)
+            csoda_mozi_vetito_fv(content.split(' ')[1])
+        # Egyebkent legyen ra valami valasz. (Lehet ettol valamivel specifikusabb kene???)
+        else:
+            message.answer("Az uzenet nem megfelelo formatumu.")
+
+
+# Itt lenne a fv, ami majd kijatsza a file-t a megfelelo csatornara.
+def csoda_mozi_vetito_fv(path):
+
 
 
 client.run(TOKEN)
